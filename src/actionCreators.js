292d729fea1f22d7f1e107967f68512245c4a513
Ivan Martinez <alcance.enlinea@gmail.com>
2019-06-25 13:38:54 -0700

    thunk and axios async call for data

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,13 +1,13 @@
-const loadProducts = () => {
-  const products = [
-    { id: 1, name: "Hipster Ultimate", price: 299, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-1.jpg" },
-    { id: 2, name: "On Motion Live", price: 99, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg" },
-    { id: 3, name: "Underground Max", price: 149, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg" },
-  ];
+import axios from 'axios';
 
-  return {
-    type: 'REPLACE_PRODUCTS',
-    products: products
+const loadProducts = () => {
+  return dispatch => {
+    return axios.get('http://localhost:3002/prodcuts').then(response => {
+      dispatch({
+        type: 'REPLACE_PRODUCTS',
+        products: response.data
+      })
+    })
   }
 }
 
