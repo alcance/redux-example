828dc60b8ec1406f1e5b72abb952f4ed1eed96a0
Ivan Martinez <alcance.enlinea@gmail.com>
2019-06-26 12:48:02 -0700

    Test async action creators with moxios stubRequest method

--- src/__tests__/actionCreators.js
+++ src/__tests__/actionCreators.js
@@ -1,7 +1,31 @@
-import { addToCart } from "../actionCreators";
+import { loadProducts, addToCart } from "../actionCreators";
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import moxios from 'moxios';
 
-const mockStore = configureStore();
+const middleware = [thunk];
+const mockStore = configureStore(middleware);
+
+beforeEach(() => moxios.install());
+afterEach(() => moxios.uninstall());
+
+it('loads products', () => {
+  const store = mockStore({ products: [] });
+
+  moxios.stubRequest('http://localhost:3002/products', {
+    status: 200,
+    response: [
+      { id: 1, name: 'Product 1', price: 100, image: ''},
+      { id: 2, name: 'Product 2', price: 200, image: ''},
+    ]
+  });
+
+  store.dispatch(loadProducts())
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions.length).toBe(1);
+    });
+});
 
 it('add to cart', () => {
   const store = mockStore({ cart: [] });
