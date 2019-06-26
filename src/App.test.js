31106a1321a8acb5487e5c9557a471fd6d6b9a51
Ivan Martinez <alcance.enlinea@gmail.com>
2019-06-25 17:53:48 -0700

    shallow test suite, enzyme, adapter, addons, and renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-15';
+import { shallow, configure } from 'enzyme';
+
+configure({
+  adapter: new Adapter()
+});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
 });
